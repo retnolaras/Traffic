cac2a3b3cc8c9688b562d0a49c38779bd9280ab9
igalna <igalna@gmail.com>
2016-02-01 10:41:40 +0000

    kcl.keepitclean.test.roadnetwork
    Added Junit jars to the classpath
    
    added test package to keep the program code and the tests separate

---
+++ TrafficSimulator/src/com/kcl/keepitclean/test/roadnetwork/TestRoadNetwork.java
@@ -0,0 +1,14 @@
+package com.kcl.keepitclean.test.roadnetwork;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRoadNetwork {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
