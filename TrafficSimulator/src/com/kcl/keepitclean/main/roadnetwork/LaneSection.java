5ac22658d9dbd2ca97d66be00cb261198137ccd6
igalna <igalna@gmail.com>
2016-02-15 14:50:55 +0000

    Rename of TestRoadNetwork to TestLaneSection
    More clearly describes the tests that happen in the Test Class.

--- TrafficSimulator/src/com/kcl/keepitclean/main/roadnetwork/LaneSection.java
+++ TrafficSimulator/src/com/kcl/keepitclean/main/roadnetwork/LaneSection.java
@@ -1,5 +1,7 @@
 package com.kcl.keepitclean.main.roadnetwork;
 
+import com.kcl.keepitclean.main.vehicle.Vehicle;
+
 /**
  * 
  * 
@@ -16,7 +18,7 @@ public interface LaneSection {
 	public boolean isOpenRight();
 	public boolean isOpeanBackward();
 	
-	/*
+	/**
 	 * method for returning whether or not a Vehicle currently occupies this 
 	 * section of road.
 	 * 
@@ -24,4 +26,21 @@ public interface LaneSection {
 	 */
 	public boolean hasVehicleOnSeciton();
 
+	/**
+	 * 
+	 * method for putting a Vehicle on this section of lane.
+	 * once this Section is occupied by a Vehicle, other Vehicles will not be able to position themselves on it.
+	 * 
+	 * @param the Vehicle type occupying this LaneSection
+	 * 
+	 */
+	public void putVehicleOnSection(Vehicle vehicle);
+	
+	/**
+	 * 
+	 * once a vehicle moves off a LaneSection it is then free to be occupied by another Vehicle
+	 * this method is to remove the pointer to the vehicle once it has left the Section
+	 * 
+	 */
+	public void removeVehicleFromSection();
 }
