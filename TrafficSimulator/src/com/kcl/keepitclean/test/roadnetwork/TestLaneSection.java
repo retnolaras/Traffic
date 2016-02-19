c609051443df9100fe961879df5058423d371a84
igalna <igalna@gmail.com>
2016-02-19 15:05:17 +0000

    Test cases for Roads
    Tests for creating roads of different lengths and types.
    Testing creating them, making sure they contain Lanes of the right type.
    Making sure they are the right width and length.
    Testing to see if you can add coordinates for the start and the end of a
    road.
    Tests for assigning a speedlimit to a road

--- TrafficSimulator/src/com/kcl/keepitclean/test/roadnetwork/TestLaneSection.java
+++ TrafficSimulator/src/com/kcl/keepitclean/test/roadnetwork/TestLaneSection.java
@@ -41,14 +41,14 @@ public class TestLaneSection {
 	
 	@Test
 	public void testVehicleOnLaneSection() {
-		laneSection = lf.getLaneSection("singlelane");
+		laneSection = lf.produceLaneSection("singlelane");
 		laneSection.putVehicleOnSection(vehicle);
 		assertEquals(laneSection.hasVehicleOnSeciton(), true);
 	}
 	
 	@Test
 	public void testRemoveVehicleFromSection() {
-		laneSection = lf.getLaneSection("singlelane");
+		laneSection = lf.produceLaneSection("singlelane");
 		laneSection.putVehicleOnSection(vehicle);
 		laneSection.removeVehicleFromSection();
 		assertEquals(laneSection.hasVehicleOnSeciton(), false);
@@ -56,19 +56,19 @@ public class TestLaneSection {
 	
 	@Test
 	public void testLaneFactoryBuildMiddleLane() {
-		laneSection = lf.getLaneSection("MIDDLELANE");
+		laneSection = lf.produceLaneSection("MIDDLELANE");
 		assertEquals(laneSection.getClass().getName(), MiddleLane.class.getName());
 	}
 	
 	@Test
 	public void testLaneFactoryBuildRightLane() {
-		laneSection = lf.getLaneSection("RIGHTLANE");
+		laneSection = lf.produceLaneSection("RIGHTLANE");
 		assertEquals(laneSection.getClass().getName(), RightLane.class.getName());
 	}
 	
 	@Test
 	public void testLaneFactoryBuildLeftLane() {
-		laneSection = lf.getLaneSection("LEFTLANE");
+		laneSection = lf.produceLaneSection("LEFTLANE");
 		assertEquals(laneSection.getClass().getName(), LeftLane.class.getName());
 	}
 
