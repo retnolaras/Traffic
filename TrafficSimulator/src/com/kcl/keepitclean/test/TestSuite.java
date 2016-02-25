bc80344f73e5cd3ea0f54f944c1b97bea31b20bf
igalna <igalna@gmail.com>
2016-02-25 14:07:48 +0000

    Added a TestSuite class for all the Test classes from sprint 1.

---
+++ TrafficSimulator/src/com/kcl/keepitclean/test/TestSuite.java
@@ -0,0 +1,19 @@
+package com.kcl.keepitclean.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import com.kcl.keepitclean.test.policy.TestPolicy;
+import com.kcl.keepitclean.test.roadnetwork.TestLaneSection;
+import com.kcl.keepitclean.test.roadnetwork.TestRoad;
+import com.kcl.keepitclean.test.session.SessionManagerSPRTest;
+import com.kcl.keepitclean.test.session.SessionManagerTest;
+import com.kcl.keepitclean.test.vehicle.TestVehicle;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TestRoad.class, TestLaneSection.class,
+					 TestPolicy.class, SessionManagerSPRTest.class,
+					 SessionManagerTest.class, TestVehicle.class})
+public class TestSuite {
+}
