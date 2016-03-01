f9b47647893fe5842e19f366ed35792857b88d14
igalna <igalna@gmail.com>
2016-03-01 16:03:23 +0000

    Implementation of PrePlannedRouteJunction
    which allows you to connect two roads, it gives you a link between the
    two roads.
    
    
    Test for it which tests that the size of the mapping is 1 after you add
    the roads to the junction.
    
    Test for null pointer when initializing an empty junction

--- TrafficSimulator/src/com/kcl/keepitclean/main/roadnetwork/junction/Junction.java
+++ TrafficSimulator/src/com/kcl/keepitclean/main/roadnetwork/junction/Junction.java
@@ -1,12 +1,12 @@
 package com.kcl.keepitclean.main.roadnetwork.junction;
 
 import java.awt.Point;
-import java.util.Set;
+import java.util.List;
 
 import com.kcl.keepitclean.main.roadnetwork.laneSection.LaneSection;
 
 public interface Junction {
 
 	
-	public Set<LaneSection> produceRoute(Point endCoordinateOfCurrentRoad, Point startCoordinateOfNextRoad);
+	public List<LaneSection> produceRoute(Point endCoordinateOfCurrentRoad, Point startCoordinateOfNextRoad);
 }
