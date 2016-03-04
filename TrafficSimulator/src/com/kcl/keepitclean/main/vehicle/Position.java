dce4b0bbda5fba3781fdae0a34775c69076e4579
Daniel Mendoza <daniel.arturo.mendoza@gmail.com>
2016-03-04 15:18:06 +0000

    fixing syntax errors

--- TrafficSimulator/src/com/kcl/keepitclean/main/vehicle/Position.java
+++ TrafficSimulator/src/com/kcl/keepitclean/main/vehicle/Position.java
@@ -1,36 +1,39 @@
-
 /*
 * Defines car position based on Lane, Road and LaneSection Indexes 
 */
-
 package com.kcl.keepitclean.main.vehicle;
-public class Position (){
-	int road=0;
-	int lane=0;
-	int laneSection=0;
+
+public class Position{
+	private int road = 0;
+	private int lane = 0;
+	private int laneSection = 0;
 	
 	
-	void setRoad(int x){
+	public void setRoad(int x){
 		road= x;
 	}
 	
-	void setLane(int x){
+	public void setLane(int x){
 		lane=x;
 	}
-	void setLaneSection(int x){
-		laneSection=x;
+	
+	public void setLaneSection(int x){
+		laneSection = x;
 	}
 	
-	int getRoad(){
+	public int getRoad(){
 		return road;
 	}
-	int getLane(){
+	
+	public int getLane(){
 		return lane;
 	}
-	int getLaneSection(){
-		return lanesection; 
+	
+	public int getLaneSection(){
+		return laneSection; 
 	}
-	int Update (int i ,int j, int  k){
+	
+	public void update (int i ,int j, int  k){
 		road = i;
 		lane=j;
 		laneSection=k;
