8a4e73d90299e5603bc71ecc11ef8a5818885291
rosiengo <thi_thanh_huong.ngo@kcl.ac.uk>
2016-03-15 09:19:55 +0000

    Revert "Revert "Revert "generate Maps"""
    This reverts commit 2a1e310297546ff00d0f5fa82b896c754d87cd84.

--- TrafficSimulator/src/com/kcl/keepitclean/main/utils/Constant.java
+++ TrafficSimulator/src/com/kcl/keepitclean/main/utils/Constant.java
@@ -10,10 +10,10 @@ package com.kcl.keepitclean.main.utils;
  * @author rosiengo
  */
 public class Constant {
-    public static int LANE_SIZE = 4;   //LANE WIDTH SIZE
+    public static int LANE_SIZE = 6;   //LANE WIDTH SIZE
     public static int VEHICLE_WIDTH = 2;
     public static int VEHICLE_HEIGHT = 2;
-    public static int PIXELS = 4;   //number of pixels per coordinate increment
+    public static int PIXELS = 10;   //number of pixels per coordinate increment
     public static int LANE_SECTION_HEIGHT = 3;  //HEIGHT OF LANE SECTION
-    public static int VEHICLE_LEFT_MARGIN = 1;
+    public static int VEHICLE_LEFT_MARGIN = 2;
 }
