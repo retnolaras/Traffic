d51c1c016d64973e20c9e6b6e926ad5b85b32300
Daniel Mendoza <daniel.arturo.mendoza@gmail.com>
2016-03-30 18:23:28 +0100

    Formatting code
    Formatting code

--- TrafficSimulator/src/com/kcl/keepitclean/main/session/Session.java
+++ TrafficSimulator/src/com/kcl/keepitclean/main/session/Session.java
@@ -1,4 +1,5 @@
 package com.kcl.keepitclean.main.session;
+
 /**
  * @author dmendoza
  */
@@ -6,57 +7,56 @@ import java.util.Observable;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 class Session extends Observable implements Runnable {
-	
+
 	private final long STEP_SIZE = 125_000_000L;
-	
+
 	private final long DEFAULT_STEP_SIZE = STEP_SIZE * 8;
 	private final long MAX_STEP_SIZE = STEP_SIZE * 24;
-			
+
 	private AtomicBoolean isOn;
 	private long stepSize;
-	
+
 	public Session() {
 		isOn = new AtomicBoolean(true);
 		stepSize = DEFAULT_STEP_SIZE;
 	}
-	
-	public void pauseSession(){
+
+	public void pauseSession() {
 		isOn.set(false);
 	}
-	
-	public void resumeSession(){
+
+	public void resumeSession() {
 		isOn.set(true);
 	}
-	
-	public void faster(){
-		synchronized(this) {
-			if(stepSize > STEP_SIZE){
+
+	public void faster() {
+		synchronized (this) {
+			if (stepSize > STEP_SIZE) {
 				stepSize -= STEP_SIZE;
 			}
 		}
 	}
-	
-	public void slower(){
-		synchronized(this) {
-			if(stepSize < MAX_STEP_SIZE){
+
+	public void slower() {
+		synchronized (this) {
+			if (stepSize < MAX_STEP_SIZE) {
 				stepSize += STEP_SIZE;
 			}
 		}
-	}	
-	
+	}
+
 	@Override
 	public void run() {
-		while(!Thread.interrupted()){
+		while (!Thread.interrupted()) {
 			long startTime = System.nanoTime();
 			long stepTime = startTime + stepSize;
-			while(isOn.compareAndSet(true, true)){
-				/*try {
-					Thread.sleep(10);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}*/
+			while (isOn.compareAndSet(true, true)) {
+				/*
+				 * try { Thread.sleep(10); } catch (InterruptedException e) {
+				 * e.printStackTrace(); }
+				 */
 				startTime = System.nanoTime();
-				if(startTime >= stepTime){
+				if (startTime >= stepTime) {
 					setChanged();
 					notifyObservers();
 					stepTime = startTime + stepSize;
