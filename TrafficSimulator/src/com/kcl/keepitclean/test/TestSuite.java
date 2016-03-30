68c851d9a6143fcb08cd3ce72ee731bc88df90f2
Daniel Mendoza <daniel.arturo.mendoza@gmail.com>
2016-03-30 17:08:29 +0100

    Junit screenshot

--- TrafficSimulator/src/com/kcl/keepitclean/test/TestSuite.java
+++ TrafficSimulator/src/com/kcl/keepitclean/test/TestSuite.java
@@ -4,21 +4,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import com.kcl.keepitclean.test.policy.TestPolicy;
-import com.kcl.keepitclean.test.roadnetwork.TestJunction;
 import com.kcl.keepitclean.test.roadnetwork.TestLaneSection;
 import com.kcl.keepitclean.test.roadnetwork.TestRoad;
-import com.kcl.keepitclean.test.roadnetwork.TestSingleLaneFourEntryFourExitOverlappingJunction;
-import com.kcl.keepitclean.test.roadnetwork.TestSingleLaneThreeEntryThreeExitOverLappingJunction;
 import com.kcl.keepitclean.test.session.SessionManagerSPRTest;
 import com.kcl.keepitclean.test.session.SessionManagerTest;
+import com.kcl.keepitclean.test.simulation.TestSimulationData;
 import com.kcl.keepitclean.test.vehicle.TestVehicle;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({TestRoad.class, TestLaneSection.class,
 					 TestPolicy.class, SessionManagerSPRTest.class,
-					 SessionManagerTest.class, TestVehicle.class,
-					 TestJunction.class, TestSingleLaneFourEntryFourExitOverlappingJunction.class,
-					 TestSingleLaneThreeEntryThreeExitOverLappingJunction.class})
+					 SessionManagerTest.class, TestVehicle.class, TestSimulationData.class})
 public class TestSuite {
 }
